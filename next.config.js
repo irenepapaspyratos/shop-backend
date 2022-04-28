a1361324c4d02e88e3d13cb6171fc0f97f2a7b2f
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-04-29 00:55:40 +0200

    Update linting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const StylelintPlugin = require('stylelint-webpack-plugin');
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.plugins.push(new StylelintPlugin());
-    return config;
+  compiler: {
+    styledComponents: true,
   },
 };
 
