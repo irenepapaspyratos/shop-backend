e982dba3dfe77866ae0325204fe8e4e850e46ff6
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-05-08 04:10:09 +0200

    Add category-filter to products-page

--- pages/products.js
+++ pages/products.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import getProducts from '../src/services/get-products';
+import getCategories from '../src/services/get-categories';
 import ProductList from '../src/components/ProductList';
 import { SWRConfig } from 'swr';
 import swrFetcher from '../src/lib/swr-fetcher';
 
 export async function getStaticProps() {
     const products = await getProducts();
+    const categories = await getCategories();
 
     return {
         props: {
             fallback: {
                 '/api/products': products,
+                '/api/categories': categories,
             },
         },
     };
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,29 +1,65 @@
 import useSWR from 'swr';
 import Product from './Product';
+import { useState } from 'react';
 
 export default function ProductList() {
-    const { data, error } = useSWR('/api/products');
+    const [selectValue, setSelectValue] = useState('All');
+    const { data: products, errorProducts } = useSWR('/api/products');
+    const { data: categories, errorCategories } = useSWR('/api/categories');
 
-    if (error) {
-        return <h3>Error: {error.message}</h3>;
+    if (errorProducts) {
+        return <h3>Error: {errorProducts.message}</h3>;
+    }
+
+    function handleSelect(val) {
+        setSelectValue(val);
     }
 
     return (
-        <ul>
-            {data.map(product => {
-                return (
-                    <li key={product.id}>
-                        <Product
-                            id={product.id}
-                            name={product.name}
-                            description={product.description}
-                            price={product.price}
-                            category={product.category}
-                            tags={product.tags}
-                        />
-                    </li>
-                );
-            })}
-        </ul>
+        <>
+            <select
+                onChange={event => {
+                    handleSelect(event.target.value);
+                }}
+            >
+                <option value="default">All</option>
+                {categories.map(category => {
+                    return (
+                        <option key={category.id} value={category.name}>
+                            {category.name}
+                        </option>
+                    );
+                })}
+            </select>
+            <ul>
+                {products.map(product => {
+                    return selectValue !== 'All' ? (
+                        product.category == selectValue && (
+                            <li key={product.id}>
+                                <Product
+                                    id={product.id}
+                                    name={product.name}
+                                    description={product.description}
+                                    price={product.price}
+                                    category={product.category}
+                                    tags={product.tags}
+                                />
+                            </li>
+                        )
+                    ) : (
+                        <li key={product.id}>
+                            <Product
+                                id={product.id}
+                                name={product.name}
+                                description={product.description}
+                                price={product.price}
+                                category={product.category}
+                                tags={product.tags}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
     );
 }
